a503af676a8fb7fe76c5886d538ab4b011fa4353
Sorawit yoktlemtae <Sorawit_yok@cmu.ac.th>
2024-01-05 15:55:50 +0700

    com

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -2,9 +2,23 @@
 using namespace std;
 
 template <typename T>
-void insertionSort(T d[],int N){
+void insertionSort(T d[], int N) {
+	for (int i = 1; i < N; i++) {
+		T current = d[i];
+    	int j = i - 1;
 
+	while (j >= 0 && d[j] < current) {
+     	 d[j + 1] = d[j];
+     	 j--;
+    }
+    d[j + 1] = current;
+
+    cout << "Pass " << i  << ":";
+    for (int f = 0; f < N; f++) cout << d[f] << " ";
+    cout << endl;
+  }
 }
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
@@ -17,3 +31,6 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
+
+
